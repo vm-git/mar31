e23ea99079deeb971f03113b179366e418ad461d
vmsrinivas82@gmail.com <vmsrinivas82@gmail.com>
2023-03-31 15:08:27 +0530

    added resources create subnets

--- input.tf
+++ input.tf
@@ -1,12 +1,25 @@
 variable "vpc_info" {
   type=object({
+    name=string
     cidr_block=string
+    subnets_cidrs=list(string)
+    subnets_names=list(string)
   })
   default = {
+    name="vpc1"
     cidr_block = "192.168.0.0/16"
+    subnets_names=["dev1","dev2"]
+    subnets_cidrs=["192.168.0.0/24","192.168.1.0/24"]
   }
 }
 variable "region" {
   type = string
     default = "ap-south-1"
 }
+
+# vpc_info={
+#    name="vpc1"
+#     cidr_block="10.10.0.0/16"
+#     subnets_names=["test1","test2"]
+#     subnets_cidrs=["10.10.0.0/24","10.10.1.0/24"]
+# }
--- network.tf
+++ network.tf
@@ -2,3 +2,11 @@ resource "aws_vpc" "ntier" {
   cidr_block = var.vpc_info.cidr_block
 }
 
+resource "aws_subnet" "subnets" {
+  count=length(var.vpc_info.subnets_names)
+  cidr_block = cidrsubnet(var.vpc_info.cidr_block,8,count.index)
+  vpc_id = aws_vpc.ntier.id
+  tags = {
+    "Name" = "${var.vpc_info.name}-${var.vpc_info.subnets_names[count.index]}"
+  }
+}
